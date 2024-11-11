e4aff61b8c2a86e05c1b39f5378fef2ad609a281
zzs <2497223499@qq.com>
2024-11-11 23:01:04 +0800

    modify

---
+++ computer_room.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+class Computer_room	{
+public:
+	int m_com_id;
+	int m_max_num;
+};
--- student.cpp
+++ student.cpp
@@ -39,7 +39,62 @@ void Student::open_menu() {
 }
 
 void Student::apply_order() {
-	
+	std::cout << "机房开放时间为周一到周五!" << std::endl;
+	std::cout << "请输入申请预约时间： " << std::endl;
+	std::cout << "1、周一" << std::endl;
+	std::cout << "2、周二" << std::endl;
+	std::cout << "3、周三" << std::endl;
+	std::cout << "4、周四" << std::endl;
+	std::cout << "5、周五" << std::endl;
+
+	int date = 0, interval = 0, room = 0;
+
+	while (true) {
+		std::cin >> date;
+		if (date >= 1 && date <= 5) {
+			break;
+		}
+		std::cout << "输入有误， 请重新输入" << std::endl;
+	}
+
+	std::cout << "请输入预约时间段: " << std::endl;
+	std::cout << "1、上午" << std::endl;
+	std::cout << "2、下午" << std::endl;
+
+	while (true) {
+		std::cin >> interval;
+		if (interval >= 1 && interval <= 2) {
+			break;
+		}
+		std::cout << "输入有误， 请重新输入" << std::endl;
+	}
+
+	std::cout << "请选择机房" << std::endl;
+	std::cout << "1号机房容量: " << v_com[0].m_max_num << std::endl;
+	std::cout << "2号机房容量: " << v_com[1].m_max_num << std::endl;
+	std::cout << "3号机房容量: " << v_com[2].m_max_num << std::endl;
+
+	while (true) {
+		std::cin >> room;
+		if (room >= 1 && room <= 3) {
+			break;
+		}
+		std::cout << "输入有误，请重新输入" << std::endl;
+	}
+
+	std::cout << "预约成功！审核中" << std::endl;
+
+	std::ofstream ofs(ORDER_FILE, std::ios::app);
+	ofs << "date:" << date << ' ';
+	ofs << "interval:" << interval << ' ';
+	ofs << "stu_id:" << this->id << ' ';
+	ofs << "stu_name:" << this->m_name << ' ';
+	ofs << "room:" << room << ' ';
+	ofs << "status:" << 1 << std::endl;
+
+	ofs.close();
+
+	system("cls");
 }
 
 void Student::show_my_order() {
